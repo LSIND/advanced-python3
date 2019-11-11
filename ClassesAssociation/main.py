78099c846751c4e7833326956b027c7d6ecdac85
Daria Chemkaeva <l_s_ind@hotmail.com>
2019-11-11 16:29:57 +0300

    Create main.py

---
+++ ClassesAssociation/main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+
+class Card:
+    """Represents a playing card."""
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        """Returns a string representation."""
+        return '%s of %s' % (self.rank, self.suit)
+
+class Deck:
+    """Represents deck of 52 cards.
+      cards: list of Card objects.
+    """
+    suits = ["spades", "hearts", "diamonds", "clubs"]
+    ranks = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+    
+    def __init__(self):
+        self.cards = []
+        for suit in range(len(self.suits)):
+            for rank in range(len(self.ranks)):
+                card = Card(self.suits[suit], self.ranks[rank])
+                self.cards.append(card)
+
+    def __str__(self):
+        """Returns a string representation."""
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+
+    def shuffle(self):
+        """Shuffle cards in the deck."""
+        random.shuffle(self.cards)
+
+if __name__ == '__main__':
+    print("Sorted deck: ")
+    deck = Deck()
+    print(deck)
+    print("-"*10)
+    print("Shuffled deck: ")
+    deck.shuffle()
+    print(deck)
