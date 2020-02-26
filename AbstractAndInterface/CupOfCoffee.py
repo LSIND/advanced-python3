ea3f6fef8085b453a41eca1dadb762e1a2900191
Daria Chemkaeva <l_s_ind@hotmail.com>
2020-02-26 17:10:13 +0300

    @properties fixed

--- AbstractAndInterface/CupOfCoffee.py
+++ AbstractAndInterface/CupOfCoffee.py
@@ -10,39 +10,45 @@ class CupOfCoffee(HotDrink):
 # constructor
     def __init__(self, bean = 'arabica'):
         if(bean=='arabica' or bean=='robusta'):
-            self._bean = bean
+            self.__bean = bean
 
 #override block
     def Milk(self, milk):
         if(0 <= milk <= 10):
             self._milk = milk
         return self._milk
-        #print("Added milk to coffee: ", self._milk)
 
     def Sugar(self, sugar):
         if(0 <= sugar <= 5):
             self._sugar = sugar
         return self._sugar
-        #print("Added sugar to coffee: ", self._sugar)
 
     def Drink(self):
-        print("Drink your coffee: ", self._bean)
+        print("Drink your coffee: ", self.__bean)
     def AddMilk(self):
         print("Added milk to coffee: ", self._milk)
     def AddSugar(self):
         print("Added sugar to coffee: ", self._sugar)
 
 #implementation block
-    _type = "test"
+    __type = "plastic"
+    __vol = 0.2
+    
     @property
     def Type(self):
-        self._type = "pl"
-        return self._type
+        return self.__type
+    @Type.setter
+    def Type(self, t):
+        self.__type = t
+
     @property
+    def Volume(self):
+        return self.__vol
+    @Volume.setter
     def Volume(self, v):
-        pass
-        #return self.volume
+        self.__vol= v
+    
     def Refill(self):
-        print("Refill cup of coffee volume")
+        print("Refill cup of coffee volume", self.__vol)
     def Wash(self):
-        print("Wash cup of coffee made of ", self._type)
+        print("Wash cup of coffee made of ", self.__type)
