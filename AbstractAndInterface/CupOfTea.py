4f24829a2923f6007ac2cba30b333651cf7ea0a6
Daria Chemkaeva <l_s_ind@hotmail.com>
2020-02-26 17:10:56 +0300

    __private fields fixed

--- AbstractAndInterface/CupOfTea.py
+++ AbstractAndInterface/CupOfTea.py
@@ -10,7 +10,7 @@ class CupOfTea(HotDrink):
 # constructor
     def __init__(self, leaf = 'black'):
         if(leaf=='green' or leaf=='black'):
-            self._leaf = leaf
+            self.__leaf = leaf
 
 #override block
     def Milk(self, milk):
@@ -24,24 +24,31 @@ class CupOfTea(HotDrink):
         return self._sugar
 
     def Drink(self):
-        print("Drink your tea: ", self._leaf)
+        print("Drink your tea: ", self.__leaf)
     def AddMilk(self):
         print("Added milk to tea: ", self._milk)
     def AddSugar(self):
         print("Added sugar to tea: ", self._sugar)
 
 #implementation block
-    _type = "test"
+    __type = "test"
+    __vol = 0.5
+
     @property
     def Type(self):
-        self._type = "glass"
-        return self._type
+        return self.__type
+    @Type.setter
+    def Type(self, t):
+        self.__type = t
+        
     @property
+    def Volume(self):
+        return self.__vol
+    @Volume.setter
     def Volume(self, v):
-        pass
-        #return self.volume
+        self.__vol= v
+        
     def Refill(self):
-        print("Refill cup of tea volume")
+        print("Refill cup of tea volume", self.__vol)
     def Wash(self):
-        print("Wash cup of tea")
-
+        print("Wash cup of TEA made of ", self.__type)
